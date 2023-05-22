ccee37f40f93ebabcf208059163aa7841d4e26cc
leonerdy-666 <69582947+leonerdy-666@users.noreply.github.com>
2023-05-22 10:11:31 -0300

    teste

--- Anhanguera/3° - Semestre/3 Programação e Desenvolvimento de Banco de Dados/Ta1/Códigos/Alteração de tabela.sql
+++
@@ -1,9 +0,0 @@
--- alterar tabela 
-ALTER TABLE ponto_tur ADD coordenada POINT;
-DROP TABLE coordenada;
-
-ALTER TABLE pais ADD interesse ENUM('0','1','2','3','4','5','6','7','8','9','10');
-describe pais;
-
-ALTER TABLE cidade ADD melhoresRest VARCHAR(300) DEFAULT'';
-describe cidade;
